43ef0c030b45b1f0c199138bc191f2f8fef33d1c
Gene Boggs <gene@ology.net>
2024-11-08 10:39:56 -0800

    Initial

--- tests.py
+++ tests.py
@@ -8,5 +8,7 @@ board.push_san("e4")
 board.push_san("d5")
 c = Coverage(board)
 cover = c.cover()
-print(json.dumps(cover, indent=2, sort_keys=True))
-print(board)
+# print(json.dumps(cover, indent=2, sort_keys=True))
+# print(board)
+
+assert c['h8']['occupant'] == "black rook"
