55cdf09e83fbe0429d74e2addd06dfebc21ef22a
Gene Boggs <gene@ology.net>
2024-11-08 10:35:31 -0800

    Initial

---
+++ tests.py
@@ -0,0 +1,12 @@
+import chess
+import json
+
+from coverage import Coverage
+
+board = chess.Board()
+board.push_san("e4")
+board.push_san("d5")
+c = Coverage(board)
+cover = c.cover()
+print(json.dumps(cover, indent=2, sort_keys=True))
+print(board)
