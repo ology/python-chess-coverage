8cc71747b80a4cedc6f635acc6e37e0e3017f371
Gene Boggs <gene@ology.net>
2024-11-08 10:26:44 -0800

    OO

--- coverage.py
+++ coverage.py
@@ -1,128 +1,131 @@
 import chess
 import json
 
-def fetch_moves(board, posn):
-    moves = []
-    for m in board.legal_moves:
-        string = str(m)
-        # print(string)
-        from_sq = string[0:2]
-        to_sq = string[2:4]
-        # print(f"F: {from_sq}, T: {to_sq}")
-        if from_sq == posn:
-            moves.append(to_sq)
-    return moves
+class Coverage:
+    def __init__(self, board):
+        self.board = board
 
-def fetch_threatens(board, moves):
-    threatens = []
-    for m in moves:
-        string = str(m)
-        square = chess.parse_square(string)
-        piece = board.piece_at(square)
-        if piece:
-            threatens.append(string)
-    return threatens
+    def fetch_moves(self, board, posn):
+        moves = []
+        for m in board.legal_moves:
+            string = str(m)
+            # print(string)
+            from_sq = string[0:2]
+            to_sq = string[2:4]
+            # print(f"F: {from_sq}, T: {to_sq}")
+            if from_sq == posn:
+                moves.append(to_sq)
+        return moves
 
-def is_king(board, posn):
-    square = chess.parse_square(posn)
-    piece = board.piece_at(square)
-    if piece.symbol().lower() == 'k':
-        return True
-    return False
+    def fetch_threatens(self, board, moves):
+        threatens = []
+        for m in moves:
+            string = str(m)
+            square = chess.parse_square(string)
+            piece = board.piece_at(square)
+            if piece:
+                threatens.append(string)
+        return threatens
 
-def fetch_protects(board, posn, square):
-    protects = []
-    fen = board.fen()
-    parts = fen.split(" ", 1)
-    flipped_fen = parts[0].swapcase()
-    fen_extra = flipped_fen + " " + parts[1]
-    board_copy = chess.Board(fen=fen_extra)
-    piece = board_copy.piece_at(square)
-    color = piece.color
-    piece.color = not color
-    board_copy.set_piece_at(square, piece)
-    board_copy.turn = not color
-    # print(posn)
-    # print(board_copy)
-    moves = fetch_moves(board_copy, posn)
-    protects = fetch_threatens(board_copy, moves)
-    for i in protects:
-        if is_king(board, i):
-            protects.remove(i)
-            break
-    return protects
+    def is_king(self, posn):
+        square = chess.parse_square(posn)
+        piece = self.board.piece_at(square)
+        if piece.symbol().lower() == 'k':
+            return True
+        return False
 
-def can_move_here(board, moves):
-    allowed = []
-    for m in moves:
-        string = str(m)
-        square = chess.parse_square(string)
-        piece = board.piece_at(square)
-        if not piece:
-            allowed.append(string)
-    return allowed
+    def fetch_protects(self, posn, square):
+        protects = []
+        fen = self.board.fen()
+        parts = fen.split(" ", 1)
+        flipped_fen = parts[0].swapcase()
+        fen_extra = flipped_fen + " " + parts[1]
+        board_copy = chess.Board(fen=fen_extra)
+        piece = board_copy.piece_at(square)
+        color = piece.color
+        piece.color = not color
+        board_copy.set_piece_at(square, piece)
+        board_copy.turn = not color
+        moves = self.fetch_moves(board_copy, posn)
+        protects = self.fetch_threatens(board_copy, moves)
+        for i in protects:
+            if self.is_king(i):
+                protects.remove(i)
+                break
+        return protects
 
-def cover(board):
-    coverage = {}
-    for square in chess.SQUARES:
-        color = board.color_at(square)
-        piece = board.piece_at(square)
-        pieces = {
-            "p": 1,
-            "n": 2,
-            "b": 3,
-            "r": 4,
-            "q": 5,
-            "k": 6,
-        }
-        name = '-'
-        if piece:
-            posn = chess.square_name(square)
-            lower = str(piece).lower()
-            index = pieces[lower]
-            name = chess.piece_name(index)
-            board.turn = color
-            if color:
-                color_name = 'white'
-            else:
-                color_name = 'black'
-            moves = fetch_moves(board, posn)
-            threatens = fetch_threatens(board, moves)
-            protects = fetch_protects(board, posn, square)
-            allowed = can_move_here(board, moves)
-            key = color_name + "_can_move_here"
-            coverage[posn] = {
-                "index": square,
-                "position": posn,
-                "color": color,
-                "occupant": f"{color_name} {name}",
-                "symbol": lower,
-                "moves": moves,
-                "threatens": threatens,
-                "protects": protects,
-                key: allowed,
+    def can_move_here(self, moves):
+        allowed = []
+        for m in moves:
+            string = str(m)
+            square = chess.parse_square(string)
+            piece = self.board.piece_at(square)
+            if not piece:
+                allowed.append(string)
+        return allowed
+
+    def cover(self):
+        coverage = {}
+        for square in chess.SQUARES:
+            color = self.board.color_at(square)
+            piece = self.board.piece_at(square)
+            pieces = {
+                "p": 1,
+                "n": 2,
+                "b": 3,
+                "r": 4,
+                "q": 5,
+                "k": 6,
             }
-    for posn in coverage:
-        c = coverage[posn]
-        if not "is_threatened_by" in c:
-            c["is_threatened_by"] = []
-        if not "is_protected_by" in c:
-            c["is_protected_by"] = []
-        for i in coverage:
-            cov = coverage[i]
-            if i == posn:
-                continue
-            if posn in cov['threatens']:
-                c["is_threatened_by"].append(i)
-            if posn in cov['protects']:
-                c["is_protected_by"].append(i)
-    return coverage
+            name = '-'
+            if piece:
+                posn = chess.square_name(square)
+                lower = str(piece).lower()
+                index = pieces[lower]
+                name = chess.piece_name(index)
+                self.board.turn = color
+                if color:
+                    color_name = 'white'
+                else:
+                    color_name = 'black'
+                moves = self.fetch_moves(self.board, posn)
+                threatens = self.fetch_threatens(self.board, moves)
+                protects = self.fetch_protects(posn, square)
+                allowed = self.can_move_here(moves)
+                key = color_name + "_can_move_here"
+                coverage[posn] = {
+                    "index": square,
+                    "position": posn,
+                    "color": color,
+                    "occupant": f"{color_name} {name}",
+                    "symbol": lower,
+                    "moves": moves,
+                    "threatens": threatens,
+                    "protects": protects,
+                    key: allowed,
+                }
+        for posn in coverage:
+            c = coverage[posn]
+            if not "is_threatened_by" in c:
+                c["is_threatened_by"] = []
+            if not "is_protected_by" in c:
+                c["is_protected_by"] = []
+            for i in coverage:
+                cov = coverage[i]
+                if i == posn:
+                    continue
+                if posn in cov['threatens']:
+                    c["is_threatened_by"].append(i)
+                if posn in cov['protects']:
+                    c["is_protected_by"].append(i)
+        return coverage
 
 def main():
     board = chess.Board()
     board.push_san("e4")
     board.push_san("d5")
-    coverage = cover(board)
+    c = Coverage(board)
+    coverage = c.cover()
     print(json.dumps(coverage, indent=2, sort_keys=True))
     print(board)
 
