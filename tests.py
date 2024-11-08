0dd0e05a307c8c51c90b27d37f3a3898dc36da35
Gene Boggs <gene@ology.net>
2024-11-08 15:36:33 -0800

    Tests

--- tests.py
+++ tests.py
@@ -12,27 +12,29 @@ cover = c.cover()
 # print(json.dumps(cover, indent=2, sort_keys=True))
 # print(board)
 
-assert cover['e4']['color'] == True
-assert cover['e4']['index'] == 28
-assert cover['e4']['is_protected_by'] == []
-assert cover['e4']['is_threatened_by'] == ["d5"]
-assert cover['e4']['moves'] == ["d5", "e5"]
-assert cover['e4']['occupant'] == "white pawn"
-assert cover['e4']['position'] == "e4"
-assert cover['e4']['protects'] == []
-assert cover['e4']['symbol'] == "P"
-assert cover['e4']['threatens'] == ["d5"]
+e4 = cover['e4']
+assert e4['color'] == True
+assert e4['index'] == 28
+assert e4['is_protected_by'] == []
+assert e4['is_threatened_by'] == ["d5"]
+assert e4['moves'] == ["d5", "e5"]
+assert e4['occupant'] == "white pawn"
+assert e4['position'] == "e4"
+assert e4['protects'] == []
+assert e4['symbol'] == "P"
+assert e4['threatens'] == ["d5"]
 
-assert cover['d5']['color'] == False
-assert cover['d5']['index'] == 35
-assert cover['d5']['is_protected_by'] == ["d8"]
-assert cover['d5']['is_threatened_by'] == ["e4"]
-assert cover['d5']['moves'] == ["e4", "d4"]
-assert cover['d5']['occupant'] == "black pawn"
-assert cover['d5']['position'] == "d5"
-assert cover['d5']['protects'] == []
-assert cover['d5']['symbol'] == "p"
-assert cover['d5']['threatens'] == ["e4"]
+d5 = cover['d5']
+assert d5['color'] == False
+assert d5['index'] == 35
+assert d5['is_protected_by'] == ["d8"]
+assert d5['is_threatened_by'] == ["e4"]
+assert d5['moves'] == ["e4", "d4"]
+assert d5['occupant'] == "black pawn"
+assert d5['position'] == "d5"
+assert d5['protects'] == []
+assert d5['symbol'] == "p"
+assert d5['threatens'] == ["e4"]
 
 assert cover['e5']['white_can_move_here'] == ["e4"]
 assert cover['e5']['black_can_move_here'] == ["e7"]
