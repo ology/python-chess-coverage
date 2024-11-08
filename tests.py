bf2b75f605dc9fc6e46905d975d65d03e40880ce
Gene Boggs <gene@ology.net>
2024-11-08 12:19:27 -0800

    Test

--- tests.py
+++ tests.py
@@ -11,4 +11,15 @@ cover = c.cover()
 # print(json.dumps(cover, indent=2, sort_keys=True))
 # print(board)
 
-assert c['h8']['occupant'] == "black rook"
+assert cover['e4']['color'] == True
+assert cover['e4']['index'] == 28
+assert cover['e4']['is_protected_by'] == []
+assert cover['e4']['is_threatened_by'] == ["d5"]
+assert cover['e4']['moves'] == ["d5", "e5"]
+assert cover['e4']['occupant'] == "white pawn"
+assert cover['e4']['position'] == "e4"
+assert cover['e4']['protects'] == []
+assert cover['e4']['symbol'] == "p"
+assert cover['e4']['threatens'] == ["d5"]
+assert cover['e5']['white_can_move_here'] == ["e4"]
+assert cover['e5']['black_can_move_here'] == ["e7"]
