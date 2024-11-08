731964b100c45e3b79191d018818db4eda49e0c2
Gene Boggs <gene@ology.net>
2024-11-08 15:33:44 -0800

    Whitespace

--- tests.py
+++ tests.py
@@ -6,6 +6,7 @@ from chess_coverage.coverage import Coverage
 board = chess.Board()
 board.push_san("e4")
 board.push_san("d5")
+
 c = Coverage(board)
 cover = c.cover()
 # print(json.dumps(cover, indent=2, sort_keys=True))
