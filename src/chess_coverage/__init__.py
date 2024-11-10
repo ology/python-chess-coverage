5c48012dd70d886cce7922d48ea3187375341f68
Gene Boggs <gene@ology.net>
2024-11-09 16:43:38 -0800

    Bump

--- src/chess_coverage/__init__.py
+++ src/chess_coverage/__init__.py
@@ -4,4 +4,4 @@ from chess_coverage.chess_coverage import Coverage
 # This is the version number of your code.  This will change each time you make
 # a new release.  The most popular approach is to use Semantic Versioning,
 # described at https://semver.org/
-__version__ = "0.1.1"
+__version__ = "0.1.2"
