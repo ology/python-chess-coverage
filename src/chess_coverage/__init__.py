4cfd826914ef190daebe4852e3ebede6b627df79
Gene Boggs <gene@ology.net>
2025-02-07 13:44:29 -0800

    Comment

--- src/chess_coverage/__init__.py
+++ src/chess_coverage/__init__.py
@@ -1,7 +1,5 @@
 # The root of the entire project
 from chess_coverage.chess_coverage import Coverage
 
-# This is the version number of your code.  This will change each time you make
-# a new release.  The most popular approach is to use Semantic Versioning,
-# described at https://semver.org/
+# This is the version number of your code. This will change each new release.
 __version__ = "0.1.4"
